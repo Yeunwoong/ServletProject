42c8fb311373e57328188781e68267f28e910ee5
Yeunwoong <60487315+Yeunwoong@users.noreply.github.com>
2020-04-16 12:26:22 +0900

    add doGet

--- src/com/hanshin/database/Base.java
+++ src/com/hanshin/database/Base.java
@@ -1,22 +1,38 @@
 package com.hanshin.database;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/base2")
 public class Base extends HttpServlet{
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		System.out.println("init called");
 	}
 	
+	
 	@Override
-	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
-		System.out.println("service called");
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		System.out.println("init called");
+		super.service(req, res);
 	}
-
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doGet called");
+		PrintWriter out = resp.getWriter();
+		out.print("<html><head><title>Test</title></head>");
+		out.print("<body><h1>doGet Have a nice day!</h1></body>");
+		out.print("</html>");
+		out.close();
+	}
 }
