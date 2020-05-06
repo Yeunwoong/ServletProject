8c3d781182571547f0a4a387cbc2b3229081f150
Yeunwoong <60487315+Yeunwoong@users.noreply.github.com>
2020-05-07 06:46:40 +0900

    add base

--- src/com/hanshin/database/Calculator.java
+++ src/com/hanshin/database/Calculator.java
@@ -11,19 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Calculator extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html;charset=UTF-8");
-		req.setCharacterEncoding("UTF-8");
-		int x = Integer.parseInt(req.getParameter("x"));
-		int y = Integer.parseInt(req.getParameter("y"));		
-		String operator = req.getParameter("operator");
-		System.out.print(operator);
-		int result = 0;
-		if(operator.equals("µ¡¼À"))
-			result = x + y;
-		else if (operator.equals("»¬¼À"))
-			result = x-y;
 		
-		resp.getWriter().printf("result is %d\n", result);
 	}	
 
 }
--- src/com/hanshin/database/Calculator2_base.java
+++ src/com/hanshin/database/Calculator2_base.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @WebServlet("/cal2")
-public class Calculator2 extends HttpServlet {
+public class Calculator2_base extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html;charset=UTF-8");
